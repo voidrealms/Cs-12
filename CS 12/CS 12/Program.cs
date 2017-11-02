cc6ac05fa0c5f84232e8b358d11f786a174e6387
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:22:52 -0400

    First commit

---
+++ CS 12/CS 12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person cPerson = new Person();
+            Person cPerson2 = new Person();
+
+            cPerson.Age = 36;
+            cPerson.Name = "Bryan";
+
+            cPerson2.Age = 18;
+            cPerson2.Name = "Heather";
+
+            Console.WriteLine(cPerson.Name + " is " + cPerson.Age + " years old");
+            Console.WriteLine(cPerson2.Name + " is " + cPerson2.Age + " years old");
+
+            Console.Read();
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: C# 12 - Classes
+Published: 9/29/2011 3:38:57 PM
+Link: https://www.youtube.com/watch?v=brfRPRVAgLg
+
+Description: Learn how to make your own classes in C#
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
